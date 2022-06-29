94d035973cf327b8107ac4c92fa2ef066dd8be62
Ednardo Paz <eds_paz@hotmail.com>
2022-06-29 15:56:13 -0300

    Init: Next-crud

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={`
+      flex h-screen justify-center items-center
+    `}>
+      <span>Texto</span>
+    </div>
+  )
+}
