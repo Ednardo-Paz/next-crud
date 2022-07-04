40444603ceb1b37af2a1757f472a2d6c1a6a1e10
Ednardo Paz <eds_paz@hotmail.com>
2022-07-04 14:25:56 -0300

    Adjust: InitalizeApp firebase

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -1,12 +1,14 @@
-import firebase from 'firebase'
-import 'firebase/firestore'
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
 
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDOmain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
-  })
+const firebaseConfig = {
+
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDOmain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+
 }
 
-export default firebase
+const app = initializeApp(firebaseConfig)
+
+export const db = getFirestore(app)
